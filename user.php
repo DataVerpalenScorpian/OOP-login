504b331afc9c14c319dec8da842fd424f661506f
DataVerpalenScorpian <99246618+DataVerpalenScorpian@users.noreply.github.com>
2023-03-30 16:13:38 +0200

    Add files via upload

---
+++ user.php
@@ -0,0 +1,159 @@
+<?php
+// Functie: classdefinitie User
+// Auteur: Toby Verpalen
+
+class User{
+// Eigenschappen
+
+    public $username;
+
+    public $email;
+
+    private $password;
+
+    function SetPassword($password){
+
+        $this->password = $password;
+    }
+
+    function GetPassword(){
+
+        return $this->password;
+    }
+
+
+    public function ShowUser() {
+
+        echo "<br>Username: $this->username<br>";
+        echo "<br>Password: $this->password<br>";  
+        echo "<br>Email: $this->email<br>";
+    }
+
+
+    public function RegisterUser(){
+
+        $status = false;
+
+        $errors=[];
+
+        if($this->username != "" || $this->password != ""){
+
+        // Check user exist
+        if(true){
+        array_push($errors, "Username bestaat al.");
+        } else {
+        // username opslaan in tabel login
+        // INSERT INTO `user` (`username`, `password`, `role`) VALUES ('kjhasdasdkjhsak', 'asdasdasdasdas', '')
+        // Manier 1
+        $status = true;
+        }
+    }
+    return $errors;
+    }
+
+
+
+
+    function ValidateUser(){
+        $errors=[];
+        if (empty($this->username)){
+            array_push($errors, "Invalid username");
+        } else if (empty($this->password)){
+            array_push($errors, "Invalid password");
+        }
+
+
+
+
+    // Test username > 3 tekens
+    return $errors;
+    }
+
+    public function LoginUser(){
+
+    // Connect database
+    require_once 'connect.php';
+
+    // Zoek user in de table user
+    // SELECT * from user WHERE username = $this->username
+
+    echo "Username:" . $this->username;
+    echo "Password:" . $this->password;
+
+    // Indien gevonden dan sessie vullen
+    if (true){
+    // Sessie vullen
+    session_start();
+    $_SESSION['username'] = $this->username;
+    $_SESSION['password'] = $this->password;
+    return true;
+    } else{
+    return false;
+    } 
+    return true;
+    }
+
+
+
+
+    // Check if the user is already logged in
+    public function IsLoggedin() {
+
+    // Check if user session has been set
+    if (isset($_SESSION['username'])) {
+        return true;
+    }
+    return false;
+}
+
+
+
+
+    public function GetUser($username) {
+        // Connect to the database
+        $servername = "localhost";
+        $dbusername = "root";
+        $dbpassword = "";
+        $dbname = "oopdb";
+
+        $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+        // Check for connection errors
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Search for user in the user table
+        $sql = "SELECT * FROM user WHERE username = '$username'";
+        $result = $conn->query($sql);
+
+        // If user is found, set object properties with values from the SELECT
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $this->username = $row["username"];
+            $this->password = $row["password"];
+        } else {
+            return NULL;
+        }
+
+        // Close the database connection
+        $conn->close();
+
+        // Return the User object
+        return $this;
+    }
+
+
+
+
+    public function Logout(){
+        session_start();
+
+        // remove all session variables
+        session_unset();
+
+        // destroy the session
+        session_destroy();
+        header('location: index.php');
+    }
+}
